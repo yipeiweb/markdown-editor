b54ced5a82d69112b7f2e25f04eda1ecedc390b8
yipeiweb <peiyiliu88@gmail.com>
2021-09-12 02:26:50 +0200

    first commit

---
+++ app/main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    mainWindow.loadFile(__dirname + '/index.html');
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+})
